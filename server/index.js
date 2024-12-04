adf8de4d44e7f5a3e63136a9c126d07445459ada
pavel <paul8paul8@mail.ru>
2024-12-04 17:02:04 +0300

    feat: add controller

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const start = async () => {
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         });
+
     } catch (e) {
         console.error(e);
     }
